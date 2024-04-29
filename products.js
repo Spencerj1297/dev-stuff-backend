e2afb6c4c19a34e5f43cca177445b730adfe1c77
Spencerj1297 <sajohnson1297@gmail.com>
2024-04-29 16:39:05 -0400

    adding new future column to data

--- products.js
+++ products.js
@@ -2,6 +2,7 @@ const products = [
   {
     id: 1,
     title: "Obsidian Glide",
+    product_type: "mouse",
     description:
       "Introducing the sleek and sophisticated ObsidianGlide, meticulously crafted with a focus on simplicity and balance. Engineered for both style and comfort, this mouse embodies understated elegance with its seamless, matte-black finish that exudes professionalism in any setting.",
     price: 39.99,
@@ -16,6 +17,7 @@ const products = [
   {
     id: 2,
     title: "Obsidian Edge65",
+    product_type: "keyboard",
     description:
       "Introducing the sleek and sophisticated ObsidianGlide, meticulously crafted with a focus on simplicity and balance. Engineered for both style and comfort, this mouse embodies understated elegance with its seamless, matte-black finish that exudes professionalism in any setting.",
     price: 119.99,
@@ -30,6 +32,7 @@ const products = [
   {
     id: 3,
     title: "Obsidian Edge75",
+    product_type: "keyboard",
     description:
       "Introducing the sleek and sophisticated ObsidianGlide, meticulously crafted with a focus on simplicity and balance. Engineered for both style and comfort, this mouse embodies understated elegance with its seamless, matte-black finish that exudes professionalism in any setting.",
     price: 139.99,
@@ -44,6 +47,7 @@ const products = [
   {
     id: 4,
     title: "PulseRift Gaming Headset",
+    product_type: "headset",
     description:
       "Introducing the PulseRift Gaming Headset – where pulse-pounding action meets immersive audio excellence. Dive into the heart of your favorite games with unparalleled sound clarity and precision. Designed for elite gamers, the PulseRift delivers an unparalleled audio experience, allowing you to hear every footstep, every explosion, and every whisper with stunning clarity. Its ergonomic design ensures hours of comfortable gameplay, while the advanced noise-canceling microphone ensures crystal-clear communication with your teammates. Unleash the power of immersive gaming with the PulseRift Gaming Headset and elevate your gaming experience to new heights.",
     price: 199.99,
@@ -58,6 +62,7 @@ const products = [
   {
     id: 5,
     title: "Eye of Sauron",
+    product_type: "accessories",
     description:
       "Behold the Eye of Sauron Key Cap – a mesmerizing homage to the iconic symbol of power from Middle-earth. Crafted with meticulous attention to detail, this key cap brings the sinister gaze of Sauron to your fingertips. Made from high-quality materials, it is not only a striking accessory for your keyboard but also a tribute to one of the most legendary villains in literary history. Elevate your typing experience with a touch of dark allure, as the Eye of Sauron watches over your every keystroke.",
     price: 29.99,
